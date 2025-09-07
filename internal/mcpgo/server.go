d0c08d98cf326559224c81e9853116eba1438e2b
GrantBirki <grant.birkinbine@gmail.com>
2025-09-07 15:55:04 -0700

    feat: update Ingredient struct to use map for ingredients field

--- internal/mcpgo/server.go
+++ internal/mcpgo/server.go
@@ -36,26 +36,26 @@ type Nutriment struct {
 
 // Ingredient represents an ingredient in a product
 type Ingredient struct {
-	CiqualFoodCode      *string      `json:"ciqual_food_code"`
-	CiqualProxyFoodCode *string      `json:"ciqual_proxy_food_code"`
-	EcobalyseCode       *string      `json:"ecobalyse_code"`
-	EcobalyseProxyCode  *string      `json:"ecobalyse_proxy_code"`
-	FromPalmOil         *string      `json:"from_palm_oil"`
-	ID                  string       `json:"id"`
-	Ingredients         []Ingredient `json:"ingredients"`
-	IsInTaxonomy        *int         `json:"is_in_taxonomy"`
-	Labels              *string      `json:"labels"`
-	Origins             *string      `json:"origins"`
-	Percent             *float64     `json:"percent"`
-	PercentEstimate     *float64     `json:"percent_estimate"`
-	PercentMax          *float64     `json:"percent_max"`
-	PercentMin          *float64     `json:"percent_min"`
-	Processing          *string      `json:"processing"`
-	Quantity            *string      `json:"quantity"`
-	QuantityG           *float64     `json:"quantity_g"`
-	Text                string       `json:"text"`
-	Vegan               *string      `json:"vegan"`
-	Vegetarian          *string      `json:"vegetarian"`
+	CiqualFoodCode      *string                  `json:"ciqual_food_code"`
+	CiqualProxyFoodCode *string                  `json:"ciqual_proxy_food_code"`
+	EcobalyseCode       *string                  `json:"ecobalyse_code"`
+	EcobalyseProxyCode  *string                  `json:"ecobalyse_proxy_code"`
+	FromPalmOil         *string                  `json:"from_palm_oil"`
+	ID                  string                   `json:"id"`
+	Ingredients         []map[string]interface{} `json:"ingredients"`
+	IsInTaxonomy        *int                     `json:"is_in_taxonomy"`
+	Labels              *string                  `json:"labels"`
+	Origins             *string                  `json:"origins"`
+	Percent             *float64                 `json:"percent"`
+	PercentEstimate     *float64                 `json:"percent_estimate"`
+	PercentMax          *float64                 `json:"percent_max"`
+	PercentMin          *float64                 `json:"percent_min"`
+	Processing          *string                  `json:"processing"`
+	Quantity            *string                  `json:"quantity"`
+	QuantityG           *float64                 `json:"quantity_g"`
+	Text                string                   `json:"text"`
+	Vegan               *string                  `json:"vegan"`
+	Vegetarian          *string                  `json:"vegetarian"`
 }
 
 // Product represents a food product from OpenFoodFacts
